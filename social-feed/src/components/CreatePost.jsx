431140f65ca3be429253d2e45d18fd9ec0828d33
Evan Kopec <ekopec0705@gmail.com>
2022-06-14 18:36:10 -0500

    Created Ability to take in user input and capture it within a text box and submit it

--- social-feed/src/components/CreatePost.jsx
+++ social-feed/src/components/CreatePost.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+
+
+
+
 
 const CreatePost = (props) => {
+
+    const [name, setName] = useState('')
+    const [post, setPost] = useState('')
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let  newPost = {
+            name: name,
+            post: post,
+        };
+
+    }
+
     return ( 
-        <form>
-            
+        <form onSubmit={handleSubmit}>
+            <label>Name</label>
+            <input type="Name" value={name} onChange={(event) => setName(event.target.value)}/>
+            <label>Post</label>
+            <input type='Post' value={post} onChange={(event) => setPost(event.target.value)}/>
+            <button type='submit'>Add</button>
         </form>
      );
 }
