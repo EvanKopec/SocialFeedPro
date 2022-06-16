cddfb287b5d56efb9f520a3314442f3af92251f8
Evan Kopec <ekopec0705@gmail.com>
2022-06-16 11:02:35 -0500

    Completed Post Component for toggling Like and Dislike button

--- social-feed/src/components/Post.jsx
+++ social-feed/src/components/Post.jsx
@@ -1,20 +1,56 @@
 import React from 'react';
-import CustomButtonLike from './CustomButtonLike';
-import CustomButtonDislike from './CustomButtonDislike';
+import './CustomButtonLike.css'
+import { useState } from 'react';
+
 
 
 const Post = ({post}) => {
 
+    const [postStatus, setPostStatus] = useState("none")
 
-    
+
+    const handleClick =(status)=>{
+        if(status === "Like"){
+            setPostStatus("liked")
+        }
+        else{
+            setPostStatus("disliked")
+        }
+    }
+
+
+
+ 
     return ( 
         <div>
             <h3>{post.name}</h3>
             <p>{post.message}</p>
-            <CustomButtonLike message="Like" />
-            <CustomButtonDislike message= "Dislike"/>
+            <div>
+                {postStatus === "none" ? 
+                <div>
+                <button className="inactive" onClick={() =>handleClick("Like")}>Like</button>
+                <button className="inactive" onClick={() =>handleClick("Dislike")}>Dislike</button>
+                </div>
+                :
+                postStatus === "liked" ?
+                <div>
+                <button className="active" onClick={() =>handleClick("Like")}>Like</button>
+                <button className="inactive" onClick={() =>handleClick("Dislike")}>Dislike</button>
+                </div>
+                :
+                <div>
+                <button className="inactive" onClick={() =>handleClick("Like")}>Like</button>
+                <button className="activated" onClick={() =>handleClick("Dislike")}>Dislike</button>
+                </div>
+        }
+            
+            </div>
+            
+            
         </div>
      );
 }
  
 export default Post;
+
+
